3f1de743f609053dc73566011f705ee770660962
Gun-Hee2 <Gun-Hee2>
2021-08-24 00:20:42 +0900

    Day1 Study Code

---
+++ Day1/CircleArea.java
@@ -0,0 +1,13 @@
+
+public class CircleArea {
+	
+	public static void main(String[] args) {
+		final double PI = 3.14;
+		
+		double radious = 10.0;
+		double circleArea = radious*radious*PI;
+		
+		System.out.println("원의 면적 = " + circleArea);
+	}
+
+}
