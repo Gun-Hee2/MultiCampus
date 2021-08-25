5b78cbc61285b46117e90076d2b467d77e758adb
Gun-Hee2 <87796075+Gun-Hee2@users.noreply.github.com>
2021-08-25 19:14:27 +0900

    Code Fix

--- Day3/PrintScanner.java
+++ Day3/PrintScanner.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-public class MainClass {
+public class PrintScanner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
